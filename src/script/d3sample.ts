bd84a70c73c68c48b670112e15b8af3f5317bfb4
RincewindWizzard <git@magierdinge.de>
2023-11-02 20:03:10 +0100

    d3 ist mir ein rätsel

--- src/script/d3sample.ts
+++ src/script/d3sample.ts
@@ -1,81 +1,225 @@
-import * as d3 from "d3";
+// import * as d3 from "d3";
+
+import * as d3Selection from "d3-selection";
+import * as d3Transition from "d3-transition";
+import * as d3Scale from "d3-scale";
+import * as d3Axis from "d3-axis"
+import * as d3Shape from "d3-shape"
+import * as d3Array from "d3-array"
+
+const d3 = {
+    ...d3Selection,
+    ...d3Transition,
+    ...d3Scale,
+    ...d3Axis,
+    ...d3Shape,
+    ...d3Array
+}
+
 
 interface Point {
     x: number
     y: number
 }
 
-const data: Point[] = []
+const step: number = 0.001
 
-for (let x = 0; x < 10; x++) {
-    data.push({x: x, y: fun(x)})
+function fun(t: number): Point {
+    const period = 2 * Math.PI
+    const growthPerPeriod = 1 - t / 100 / period
+
+    const circles = Math.ceil(t * step)
+    const phi = t * step * period
+    return {
+        x: phi,
+        y: Math.cos(phi * circles * 3)
+    }
+}
+// function fun(t: number): Point {
+//     const period = 2 * Math.PI
+//     const growthPerPeriod = 1 - t / 100 / period
+//
+//     const circles = Math.ceil(t * step)
+//     const phi = t * step * period
+//     return {
+//         x: Math.sin(phi * circles * 5),
+//         y: Math.cos(phi * circles * 3)
+//     }
+// }
+
+class LinePlot {
+    svg: d3.Selection<SVGGElement, any, any, any>
+    transitionDuration: number = 1000
+    width: number
+    height: number
+    xScale: d3.ScaleLinear<number, number>
+    yScale: d3.ScaleLinear<number, number>
+    xAxis: d3.Axis<d3.NumberValue>
+    yAxis: d3.Axis<d3.NumberValue>
+    svgAxisX: any
+    svgAxisY: any
+    line: d3.Line<Point>
+    // path: d3.Selection<any, any, any, any>
+
+
+    constructor(container: HTMLDivElement) {
+        const margin = {top: 10, right: 30, bottom: 30, left: 60}
+        this.width = container.offsetWidth - margin.left - margin.right
+        this.height = container.offsetHeight - margin.top - margin.bottom;
+
+        this.svg = d3.select(container)
+            .append("svg")
+            .attr("width", container.offsetWidth)
+            .attr("height", container.offsetHeight)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        // clipping of the path
+        this.svg.append("defs")
+            .append("clipPath")
+            .attr("id", "line-clip") // Eindeutige ID für das Clipping
+            .append("rect")
+            .attr("width", this.width) // Breite des Clippings
+            .attr("height", this.height);
+
+        this.xScale = d3.scaleLinear()
+            .domain([0, 1])
+            .range([0, this.width])
+        this.yScale = d3.scaleLinear()
+            .domain([0, 1])
+            .range([this.height, 0])
+
+
+        this.xAxis = d3.axisBottom(this.xScale)
+        this.yAxis = d3.axisLeft(this.yScale)
+
+        this.svgAxisX = this.svg
+            .append("g")
+            .attr('class', 'axis axisX')
+            .attr("transform", `translate(0, ${this.height})`)
+            .call(this.xAxis)
+
+        this.svgAxisY = this.svg
+            .append("g")
+            .attr('class', 'axis axisY')
+            .call(this.yAxis)
+
+
+        this.line = d3.line<Point>()
+            .x(d => this.xScale(d.x))
+            .y(d => this.yScale(d.y));
+
+    }
+
+    updateScale(data: Point[]) {
+        this.xScale
+            .domain([
+                d3.min(data, (d) => d.x) || 0,
+                d3.max(data, (d) => d.x) || 1,
+            ])
+            .range([0, this.width])
+
+        this.yScale
+            .domain([
+                d3.min(data, (d) => d.y) || 0,
+                d3.max(data, (d) => d.y) || 1
+            ])
+            .range([this.height, 0])
+    }
+
+    update(data: Point[]) {
+        const lineData = this.svg
+            .selectAll(".line")
+            .data([data])
+
+
+        lineData
+            .transition()
+            .duration(this.transitionDuration)
+            .attr("d", this.line);
+
+
+        lineData
+            .exit()
+            .transition()
+            .duration(this.transitionDuration)
+            .remove();
+
+
+        lineData
+            .enter()
+            .append("path")
+            .attr('class', 'line') // Verwende eine Klasse, um die Linie zu klassifizieren
+            .attr("clip-path", "url(#line-clip)")
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 2)
+            .transition()
+            .duration(this.transitionDuration)
+            .attr("d", this.line);
+
+
+        this.updateScale(data);
+
+
+        this.svgAxisX
+            .transition()
+            .duration(this.transitionDuration)
+            .call(this.xAxis);
+
+        this.svgAxisY
+            .transition()
+            .duration(this.transitionDuration)
+            .call(this.yAxis);
+    }
 }
 
-const container = d3.select("#diagram");
-const table = container.append("table")
-const tbody = table.append("tbody");
 
-const addButton = document.getElementById('btn-add')
-const removeButton = document.getElementById('btn-remove')
-
-function fun(x: number): number {
-    return x * x
-}
-
-function setup() {
-    table.attr('class', 'table is-striped is-hoverable')
-        .append("thead")
-        .append("tr")
-        .selectAll("th")
-        .data(["x", "f(x)"])
-        .enter()
-        .append("th")
-        .text(d => d)
-
-    update()
-}
+// function update(path: any, line: any) {
+//     path.datum(data)
+//         .transition()
+//         .duration(1000)
+//         .attr("d", line);
+// }
 
 
-function update() {
-    console.log(data)
+document.addEventListener("DOMContentLoaded", () => {
+    const container: HTMLDivElement | null = document.getElementById('diagram') as HTMLDivElement
+    const addButton: HTMLButtonElement | null = document.getElementById('btn-add') as HTMLButtonElement
+    const removeButton: HTMLButtonElement | null = document.getElementById('btn-remove') as HTMLButtonElement
 
-    const tr = tbody
-        .selectAll("tr")
-        .data(data)
+    if (container) {
+        const data: Point[] = []
 
-    tr.exit()
-        .transition()
-        .duration(100)
-        .style("opacity", 0)
-        .remove()
+        for (let i = 0; i < 100; i++) {
+            data.push(fun(data.length))
+        }
 
-    const newTr = tr
-        .enter()
-        .append("tr")
-
-    newTr
-        .style("opacity", 0)
-        .transition()
-        .duration(100)
-        .style("opacity", 1)
+        const plot = new LinePlot(container)
+        plot.update(data)
 
 
-    appendRows(newTr)
-}
+        // const handle = setInterval(() => {
+        //     data.push(fun(data.length))
+        //     data.shift()
+        //     plot.update(data)
+        // }, 100)
 
-function appendRows(tr: d3.Selection<any, any, any, any>) {
-    tr.append("td").text(d => d.x);
-    tr.append("td").text(d => d.y);
-}
 
-addButton?.addEventListener('click', () => {
-    data.push({x: data.length, y: fun(data.length)})
-    update()
-})
+        addButton?.addEventListener('click', () => {
+            for (let i = 0; i < 10; i++) {
+                data.push(fun(data.length))
+            }
+            plot.update(data)
+        })
 
-removeButton?.addEventListener('click', () => {
-    data.pop()
-    update()
-})
-
-document.addEventListener("DOMContentLoaded", setup);
+        removeButton?.addEventListener('click', () => {
+            for (let i = 0; i < 10; i++) {
+                data.shift()
+            }
+            plot.update(data)
+            // plot.update(data)
+            // clearInterval(handle)
+        })
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
-
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 
@@ -12,7 +12,7 @@ module.exports = {
         main: './src/index.ts',
     },
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
@@ -37,6 +37,7 @@ module.exports = {
         ],
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './src/templates/index.hbs',
             filename: 'index.html',
@@ -47,6 +48,13 @@ module.exports = {
     ],
     resolve: {
         extensions: ['.js', '.ts'],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+    performance: {
+        hints: false,
     },
     devServer: {
         static: path.join(__dirname, 'dist'),
